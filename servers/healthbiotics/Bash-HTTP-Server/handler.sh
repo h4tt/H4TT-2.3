5ae837a945ac090469568b4b6d64df01e954c77a
Forest Anderson <forestkzanderson@gmail.com>
2019-06-18 19:26:14 -0400

    Added most of the server

---
+++ servers/healthbiotics/Bash-HTTP-Server/handler.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+response_code(){
+  echo -n "HTTP/1.1 "
+  case "$1" in
+    200)
+      echo "200 OK"
+      ;;
+    400)
+      echo "400 Bad Request"
+      ;;
+    403)
+      echo "403 Forbidden"
+      ;;
+    404)
+      echo "404 Not Found"
+      ;;
+    *)
+      echo "418 I'm a Teapot"
+      ;;
+  esac
+}
+
+MIME_header(){
+  case "$file" in
+    *.png)
+      echo "image/png"
+      ;;
+    *.jpg|*.jpeg)
+      echo "image/jpg"
+      ;;
+    *.html)
+      echo "text/html"
+      ;;
+    *.css)
+      echo "text/css"
+      ;;
+    *.js)
+      echo "application/javascript"
+      ;;
+    *.json)
+      echo "application/json"
+      ;;
+    *.pdf)
+      echo "application/pdf"
+      ;;
+    *)
+      echo "text/html"
+  esac
+}
+
+print_headers(){
+  response_code $1
+  echo "Date: $(date)"
+  echo "Connection: close"
+  echo "Server: NatsServer"
+  echo "Accept-Ranges: bytes"
+  echo -n "Content-Type: "
+  MIME_header
+  echo ""
+}
+
+parse_URL(){
+  split_array=($1)
+  parsed_url=${split_array[1]#?}
+}
+
+throw_400(){
+  print_headers 400
+  echo "Bad request!"
+  exit 0
+}
+
+throw_404(){
+  print_headers 404
+  echo "Bad path, sorry :("
+  exit 0
+}
+
+grab_resource(){
+  cd ~/server/public_html
+  IFS='/' read -ra path_arr <<< "$1"
+  IFS=''
+  len=${#path_arr[@]}
+  count=0
+  for i in "${path_arr[@]}"; do
+    ((count++))
+    if [ $count -eq $len ]; then
+      if [ -f "$i" ]; then
+        file=$i
+      elif [ -f "${i}/index.html" ]; then
+        cd $i
+        file="index.html"
+      else
+        throw_404
+      fi
+    else
+      cd $i || throw_404
+    fi
+  done
+}
+
+read -r line || throw_400
+
+prep="${line}?"
+url=$(echo $prep | cut -d'?' -f1)
+params=$(echo $prep | cut -d'?' -f2)
+step=$(echo $params | cut -d'=' -f2)
+stopnum=$(echo $step | cut -d' ' -f1)
+
+
+parse_URL "$url"
+
+if [ -z "$parsed_url" ]; then
+  grab_resource "index.html"
+else
+  grab_resource $parsed_url
+fi
+
+print_headers 200
+
+php -e $file $stopnum
